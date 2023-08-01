4a7946f7c8f45777a7cae34cf03d47aadc729888
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-01 23:59:18 +0530

    [Nithin] Add : Updation of Hashtable Frequency

--- HashAndTrees/Program.cs
+++ HashAndTrees/Program.cs
@@ -13,14 +13,14 @@ public class Program
             switch (number)
             {
                 case 1:
-                    Console.WriteLine("HashTable \n!");
+                    Console.WriteLine(" => HashTable ");
                     MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-                    hash.Add("0", "To");
-                    hash.Add("1", "be");
-                    hash.Add("2", "or");
-                    hash.Add("3", "not");
-                    hash.Add("4", "to");
-                    hash.Add("5", "be");
+                    hash.Add("0", "a");
+                    hash.Add("1", "b");
+                    hash.Add("2", "yes");
+                    hash.Add("3", "no");
+                    hash.Add("4", "y");
+                    hash.Add("5", "z");
                     string hash5 = hash.Get("5");
                     Console.WriteLine("5th index value : " + hash5);
                     hash.Remove("2");
@@ -29,7 +29,7 @@ public class Program
                     Console.WriteLine("\n");
                     break;
                 case 2:
-                    Console.WriteLine("HashTable!\n");
+                    Console.WriteLine(" => BinarySearch ");
                     BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
                     binarySearch.Insert(30);
                     binarySearch.Insert(70);
@@ -51,6 +51,9 @@ public class Program
                     break;
                 case 3:
                     flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter the Correct Option to get 'i/p' !!");
                     break;
             }
 
